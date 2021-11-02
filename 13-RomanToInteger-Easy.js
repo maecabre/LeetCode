16c8f7b437f07d7c62d3bbb7266e7bf5e3ab5b53
Mario Cabrera <mccabrera96@gmail.com>
2021-11-02 12:01:42 -0700

    Fixing formatting

--- 13-RomanToInteger-Easy.js
+++ 13-RomanToInteger-Easy.js
@@ -1,22 +1,66 @@
-// Given a roman numeral, convert it to an integer.
+/*
+
+*******************************************************************************
+13-RomanToInteger-Easy.js
+*******************************************************************************
+
+// Description ******************************************************
+
+	Symbol       Value
+	I             1
+	V             5
+	X             10
+	L             50
+	C             100
+	D             500
+	M             1000
+
+	Given a roman numeral, convert it to an integer.
+
+
+// Examples *********************************************************
+
+	Example 1:
+		Input: s = "III"
+		Output: 3
+
+	Example 2:
+		Input: s = "IV"
+		Output: 4
+
+	Example 3:
+		Input: s = "IX"
+		Output: 9
+
+	Example 4:
+		Input: s = "LVIII"
+		Output: 58
+		Explanation: L = 50, V= 5, III = 3.
+
+	Example 5:
+		Input: s = "MCMXCIV"
+		Output: 1994
+		Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+
+*/
 
 /**
  * @param {string} s
  * @return {number}
  */
 var romanToInt = function(s) {
-    var integer = 0;
-    var lastNum = 0;
-    var currentNum = 0;
+	var integer = 0;
+	var lastNum = 0;
+	var currentNum = 0;
 
-    // read string right to left
-    for(var i = s.length - 1; i > -1; i--){
+	// read string right to left
+	for(var i = s.length - 1; i > -1; i--){
 
-        if(i < s.length - 1){
-            lastNum = currentNum;
-        }
+		if(i < s.length - 1){
+			lastNum = currentNum;
+		}
 
-        switch(s[i]){
+		switch(s[i]){
 			case "I":
 				currentNum = 1;
 				break;
@@ -44,13 +88,13 @@ var romanToInt = function(s) {
 		}
 
 		if(currentNum >= lastNum){
-		    integer += currentNum;
+			integer += currentNum;
 		} else if(currentNum < lastNum){
-		    integer -= currentNum;
+			integer -= currentNum;
 		}
 
-    }
+	}
 
-    console.log(integer);
-    return integer;
+	console.log(integer);
+	return integer;
 };
