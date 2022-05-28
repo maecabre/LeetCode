9036938fb40690152e0f13ce727a5b017001eaab
Mario Cabrera <mccabrera96@gmail.com>
2022-05-28 11:37:29 -0700

    Adding 704

---
+++ 704-BinarySearch-Easy.js
@@ -0,0 +1,56 @@
+/*
+
+*******************************************************************************
+121-BinarySearch-Easy.js
+*******************************************************************************
+
+// Description ******************************************************
+
+Given an array of integers nums which is sorted in ascending order,
+and an integer target, write a function to search target in nums.
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+
+// Examples *********************************************************
+
+Example 1:
+	Input: nums = [-1,0,3,5,9,12], target = 9
+	Output: 4
+	Explanation: 9 exists in nums and its index is 4
+
+Example 2:
+	Input: nums = [-1,0,3,5,9,12], target = 2
+	Output: -1
+	Explanation: 2 does not exist in nums so return -1
+
+*/
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+	let left = 0;
+	let right = nums.length - 1;
+	let middle;
+
+	// Adjust left and right pointers until target matches middle
+	// Return -1 otherwise
+	while (left <= right){
+
+		middle = Math.floor((left + right)/2);
+
+		if(target < nums[middle]){
+			right = middle - 1;
+		} else if(target > nums[middle]){
+			left = middle + 1;
+		} else if(target == nums[middle]){
+			return middle;
+		}
+	}
+
+	return -1;
+};
